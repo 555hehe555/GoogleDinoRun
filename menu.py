da2c763636cc3365a6ebd8147994248119ddc151
555hehe555 <123888406+555hehe555@users.noreply.github.com>
2023-11-14 15:54:38 +0200

    add menu and new font

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 
 pygame.init()
 pygame.mixer.init()
@@ -8,12 +9,15 @@ SCREEN_WIDTH = 750
 SCREEN_HEIGHT = 500
 GROUND_HEIGHT = 30
 SAND_HEIGHT = 10
+font1 = ".\Press_Start_2P\PressStart2P-Regular.ttf"
 
 # Load images
 dinosaur_img = pygame.image.load("./imegs/dino/dino_k.png")
 sand_img = pygame.image.load("./imegs/textures/sand.png")
 sky_img = pygame.image.load("./imegs/textures/sky.png")
 sky_img = pygame.transform.scale(sky_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
+musik_on_img = pygame.image.load("./imegs/buttons/pause.png")
+musik_off_img = pygame.image.load("./imegs/buttons/dth.png")
 
 # Background color and window initialization
 background = (153, 0, 153)
@@ -33,7 +37,7 @@ class Menu:
         self._current_option_index = 0
 
     def append_option(self, option, callback):
-        option_surface = pygame.font.Font(None, 40).render(option, True, (255, 255, 255))
+        option_surface = pygame.font.Font(font1, 40).render(option, True, (255, 255, 255))
         self._options.append(option_surface)
         self._callbacks.append(callback)
 
@@ -50,35 +54,50 @@ def quit_game():
 menu = Menu()
 
 # Add options to the menu
-menu.append_option("Start Game", lambda: print("Starting the game..."))
-menu.append_option("Quit", quit_game)
-menu.append_option("about developers", lambda: print("about developers..."))
+def create_menu_option():
+    from GoogleDinoRun_test import game_cicle
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            if event.key == pygame.K_UP:
-                menu.switch(-1)
-            elif event.key == pygame.K_DOWN:
-                menu.switch(1)
-            elif event.key == pygame.K_SPACE:
-                menu.select()
+    menu.append_option("Start Game", game_cicle)
+    menu.append_option("Quit", quit_game)
+    menu.append_option("about developers", lambda: print("about developers..."))
 
-    mw.blit(sky_img, (0, 0))
-    mw.blit(sand_img, (0, SCREEN_HEIGHT - GROUND_HEIGHT - SAND_HEIGHT + sand_offset))
+create_menu_option()
 
-    # Draw the menu
-    for i, option_surface in enumerate(menu._options):
-        option_rect = option_surface.get_rect()
-        option_rect.topleft = (100, 70 + i * 70)
-        if i == menu._current_option_index:
-            pygame.draw.rect(mw, (9, 100, 0), option_rect)
-        mw.blit(option_surface, option_rect)
+def menu_run():
+    global running
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.key == pygame.K_UP:
+                    menu.switch(-1)
+                elif event.key == pygame.K_DOWN:
+                    menu.switch(1)
+                elif event.key == pygame.K_SPACE:
+                    menu.select()
 
-    pygame.display.flip()
+        mw.blit(sky_img, (0, 0))
+        mw.blit(sand_img, (0, SCREEN_HEIGHT - GROUND_HEIGHT - SAND_HEIGHT + sand_offset))
+        mw.blit(musik_on_img, (700, 10))
 
-pygame.quit()
+        # Draw the menu
+        for i, option_surface in enumerate(menu._options):
+            option_rect = option_surface.get_rect()
+            option_rect.topleft = (100, 70 + i * 70)
+            if i == menu._current_option_index:
+                pygame.draw.rect(mw, (9, 100, 0), option_rect)
+            mw.blit(option_surface, option_rect)
+
+        pygame.display.flip()
+
+    sys.exit()
+    pygame.quit()
+
+menu_run()
+# if show_menu != False:
+#     print("menu is running")
+#     menu_run()
